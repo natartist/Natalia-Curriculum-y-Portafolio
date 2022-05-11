3af48f411adf3e09845dc0e9a074e4b9b529e506
Natalia <nata.araya@gmail.com>
2022-05-11 16:33:40 -0400

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <nav class="navbar fixed-top navbar-light" style="background-color: #DDF7F2;">
+
+  <div class="container-fluid">
+    <span class="navbar-text">  
+      Natalia Araya | FrontEnd Developer 
+    </span>
+    
+    
+<ul>
+     <div class="nav-item">
+    <router-link class="nav-link" to="/">Home</router-link>
+     </div>
+     <div class="nav-item">
+     <router-link class="nav-link" to="/Portafolio">Portafolio</router-link>
+     </div>
+</ul>
+    
+    
+</div>
+</nav>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+nav {
+  box-shadow: 2px 2px 5px 0;
+  /* border-radius: 10px; */
+  font-family: 'Quicksand', sans-serif;
+  font-size: 20px;
+  padding: 0 124px 0 124px;
+}
+
+ul {
+    display: flex;
+    align-items: flex-end;
+    
+}
+.nav-item {
+
+  color: grey;
+ 
+}
+
+.nav-link {
+
+  color: grey;
+}
+
+
+.nav-link:focus, .nav-link:hover {
+    color: #eed5ee;
+}
+
+
+</style>
+
+
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,613 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row fila-1">
+      <div class="caja1 col-md-8 col-xs-8" style="background-color: #f7efdd">
+        <h2>Quién soy</h2>
+        <p>
+          Profesional con más de 18 años de experiencia en área de
+          comunicaciones y coordinación de proyectos, adentrándose en las nuevas
+          tecnologías a través del desarrollo web (FrontEnd Developer). Enfocada
+          en la satisfacción del cliente y cumplimiento de metas, por medio de
+          la creación de páginas web acordes a los requerimientos del área. Con
+          interés de aprender, habilidades en trabajo en equipo, manejo bajo
+          presión, resiliente y con competencias relacionadas con comunicación
+          verbal y escrita para generar un trabajo de excelencia y eficiente.
+        </p>
+
+        <!-- Button trigger modal -->
+        <button
+          type="button"
+          class="btn"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal1"
+        >
+          Ver más
+        </button>
+
+        <!-- Modal -->
+        <div
+          class="modal fade"
+          id="exampleModal1"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Quién soy</h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ut
+                at quae vel alias labore sint? Magni eos rem itaque iure tenetur
+                ea quibusdam, eveniet alias laudantium impedit dolores quidem!
+              </div>
+              <!--/modal-->
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="caja2 col-md-3 col-xs-6 mt-3"
+        style="background-color: #e3f6ff"
+      >
+        <h2>FrontEnd Skills</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>VueJs</li>
+          <li>NodeJs</li>
+          <li>Git</li>
+          <li>Saas</li>
+          <li>Gulp</li>
+          <li>WebPack</li>
+          <li>JSon</li>
+          <li>Firebase</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row fila-2">
+      <div
+        class="caja3 col-md-3 col-xs-6 mt-3"
+        style="background-color: #f1edfc"
+      >
+        <h2>Redes Sociales</h2>
+        <a
+          title="Linkedin"
+          href="https://www.linkedin.com/in/natalia-araya-frontend/"
+        >
+          <img class="rrss" src="@/assets/linkedin.png" alt="Linkedin" />
+          <p>Linkedin</p>
+        </a>
+
+        <a title="GitHub" href="https://github.com/natartist">
+          <img class="rrss" src="@/assets/github.png" alt="GitHub" />
+          <p>GitHub</p>
+        </a>
+
+        <a
+          title="Twitter"
+          href="https://www.linkedin.com/in/natalia-araya-frontend/"
+        >
+          <img class="rrss" src="@/assets/twitter.png" alt="Twitter" />
+          <p>Twitter</p>
+        </a>
+      </div>
+
+      <div
+        class="caja4 col-md-8 col-xs-10 mt-3"
+        style="background-color: #eed5ee"
+      >
+        <h2>Estudios</h2>
+        <p>
+          <b>• Desarrolladora FrontEnd.</b> Bootcamp Talento Digital. Sence.
+          Impartido por Kibernum Academy TI. 2021-2022.<br /><b
+            >• Titulada de Periodismo</b
+          >, Pontificia Universidad Católica de Valparaíso. Egreso, 2000.<br />
+          • <b>Titulada de Fotografía profesional</b>, Instituto Profesional
+          INCA-CEA, Viña del Mar. Egreso, 2004.
+        </p>
+      </div>
+    </div>
+
+    <div class="row fila-3 mt-4">
+      <div
+        class="caja5 col-md-12 col-xs-12 mt-3"
+        style="background-color: #fdf5fc"
+      >
+        <h2>Experiencia Laboral</h2>
+        <h3>(últimos 3 trabajos)</h3>
+        <p>
+          <b>• Tryo Teatro Banda. Compañía teatral. 2018-2021</b>
+          <br /><i
+            >Producción teatral, comunicaciones, gestión de redes sociales y
+            formulación y coordinación de proyectos.</i
+          ><br /><br />
+          Apoyo en la producción, manejo de redes y coordinación de proyectos en
+          esta compañía de teatro, una de las más importantes del país
+          (adjudicación de fondo “Teatro Itinerante” de Ministerio de las
+          Culturas y de las Artes, 2018), trabajo consistente en coordinar
+          externa e internamente todas las etapas para lograr una correcta
+          ejecución de cada presentación y proyecto.<br />
+          Responsabilidades y logros: <br />- Contactar, coordinar y comunicar
+          requerimientos estratégicos, comunicacionales y técnicos con cada
+          espacio de presentación de la compañía, logrando la correcta
+          ejecución, con alto número de beneficiarios en cada función. <br />-
+          Coordinar internamente con el equipo los calendarios de cada ensayo y
+          presentación, acceso de movilización, alimentación y alojamiento
+          durante las giras teatrales, generando una armonía y correcto
+          desempeño de la labor teatral del grupo. <br />- Difundir diversos
+          eventos de la compañía a través de Redes sociales(Facebook, twitter e
+          Instagram) y comunicados de prensa, logrando incrementar el número de
+          seguidores e informar pertinentemente las características de la obra a
+          presentar, espacio y horarios, previos a cada presentación. <br />-
+          Formular y postular a diversos fondos para la generación de nuevos
+          proyectos e itinerancias, logrando la adjudicación e implementación de
+          varios de ellos.
+        </p>
+        <p>
+          <b>• Molécula. Radio Online. 2011- 2017</b>
+          <br /><i
+            >Producción radial, gestión de redes sociales, edición
+            fotográfica.</i
+          ><br /><br />
+          Encargada de sección “La foto del día”, donde se mostraban trabajos de
+          distintos fotógrafos invitados. Gracias al desempeño en esta área, se
+          abrieron oportunidades y se adquirieron nuevas responsabilidades, como
+          productora de los distintos programas en vivo que se transmitían por
+          señal online y community manager, para comunicar las distintas
+          actividades a realizar.<br />Responsabilidades y logros: <br />-
+          Contactar y coordinar con distintos entrevistados su participación a
+          los diversos programas radiales a mi cargo, en el ámbito de la
+          producción, logrando una nutrida carta de invitados. Coordinación
+          interna entre el equipo de trabajo, logrando salir al aire siempre en
+          los horarios establecidos, sorteando toda clase de contingencias.
+          <br />- Manejo de redes sociales de plataforma online (twitter,
+          Facebook e Instagram) logrando obtener una comunidad sólida de
+          radioescuchas y comunicando adecuadamente cada actividad o programa.
+          <br />- Convocar a la comunidad de fotógrafos en Chile y el
+          extranjero, para su participación en “La foto del Día”, espacio donde
+          se logró dar a conocer el trabajo de cientos de profesionales y
+          amateurs, en nuestra plataforma.
+        </p>
+
+        <p>
+          <b>• Universidad Tecnológica INACAP. 2008- 2011 </b><br />
+          <i>Docencia</i><br /><br />Docente de los ramos Fotografía Documental,
+          Fotografía Digital y Taller de Laboratorio en Blanco y Negro en
+          carreras de Fotografía y Cine, de la Universidad Tecnológica Inacap.
+          Además, se realizaron mentorías, como Profesora Guía en diversos
+          procesos de titulación.<br />
+          Responsabilidades y logros:<br />
+          - Aportar conocimientos a los alumnos de cada ramo, realizando una
+          capacitación y generación de competencias para su posterior desarrollo
+          laboral.<br />
+          - Comprometer diversas actividades que generaran una adquisición de
+          conocimientos por parte de los alumnos, a través de ejercicios
+          prácticos.<br />
+          - Evaluar de manera competente los conocimientos y capacidades de cada
+          estudiante generando siempre vínculos para sacar todo el potencial de
+          los alumnos, con crecimiento en sus competencias.
+        </p>
+
+        <!-- Button trigger modal -->
+        <button
+          type="button"
+          class="btn"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal2"
+        >
+          Ver más
+        </button>
+
+        <!-- Modal -->
+        <div
+          class="modal fade"
+          id="exampleModal2"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Experiencia laboral</h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, nostrum cupiditate? Ut quisquam nostrum eos delectus? Deleniti iure porro nam enim, temporibus et earum blanditiis expedita, sed voluptatum, similique quas!</div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row fila-4">
+      <div
+        class="caja6 col-md-12 col-xs-12 mt-3"
+        style="background-color: #eefce2"
+      >
+        <h2>Otros Intereses</h2>
+        <p>
+          Me gustan muchísimo las actividades manuales. Soy ceramista, tengo un
+          horno de baja temperatura que me permite realizar todo el proceso
+          alfarero. También me gusta dibujar, pintar, bordar, tejer y el
+          scrapbooking. La literatura, la música y el buen cine también me
+          apasionan.
+        </p>
+
+        <!-- Button trigger modal -->
+        <div class="areabtn">
+          <button
+            type="button"
+            class="btn"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal3"
+          >
+            Ver más
+          </button>
+        </div>
+
+        <!-- Modal -->
+        <div
+          class="modal fade"
+          id="exampleModal3"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Otros intereses</h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem facere temporibus ut sequi rerum quam tempore! Quam dolor illo minus alias ducimus obcaecati rem optio praesentium ut soluta, architecto temporibus?</div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
+ 
+  <footer>
+    <div class="row fila-5">
+      <!-- formulario -->
+      <form ref="form" @submit.prevent="sendEmail">
+        <div class="caja7 col-md-5 col-xs-6">
+          <div class="mb-3">
+            <label for="user" class="form-label">Nombre</label>
+            <input
+              type="text"
+              class="form-control"
+              id="user"
+              name="user_name"
+            />
+            <div id="nameHelpBlock" class="form-text">Escribe tu nombre.</div>
+          </div>
+
+          <div class="mb-3">
+            <label for="mail" class="form-label">Email</label>
+            <input
+              type="email"
+              class="form-control"
+              id="mail"
+              name="user_email"
+            />
+            <div id="nameHelpBlock" class="form-text">Escribe tu email.</div>
+          </div>
+        </div>
+
+        <div class="caja8 col-md-5 col-xs-6">
+          <div class="mb-3">
+            <label for="mail" class="form-label">Comentarios</label>
+            <div class="form-floating">
+              <textarea
+                name="message"
+                class="form-control"
+                style="height: 120px"
+              ></textarea>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-light">Enviar</button>
+        </div>
+      </form>
+      <!-- fin del formulario -->
+  
+    </div>
+  </footer>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import emailjs from "emailjs-com";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+
+  components: {},
+  methods: {
+    sendEmail() {
+      emailjs
+        .sendForm(
+          "service_id5i86r",
+          "template_bxe159k",
+          this.$refs.form,
+          "GTXCMTTS14DOrzPT8"
+        )
+        .then(
+          (results) => {
+            console.log("success!", results.text);
+          },
+          (error) => {
+            console.log("failed...", error.text);
+          }
+        );
+    },
+  },
+};
 </script>
+
+<style scoped>
+.container {
+  width: 1000px;
+  margin-top: 20px;
+  padding: 100px 40px 20px 40px;
+  background-image: url("@/assets/entorno.png");
+  background-attachment: fixed;
+  background-repeat: repeat;
+  background-size: 400px;
+}
+
+.fila-1 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.fila-2 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.fila-3 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.fila-4 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* width: 650px; */
+}
+
+.fila-5 {
+  display: flex;
+  align-items: center;
+  justify-content: space space-between;
+  /* width: 650px; */
+}
+
+.caja1 {
+  /* margin-left: 15px; */
+  border-radius: 15px;
+  box-shadow: 2px 2px 5px 0;
+  padding: auto;
+}
+
+
+.caja2 {
+  border-radius: 15px;
+  box-shadow: 2px 2px 5px 0;
+  padding: auto;
+}
+
+.caja3 {
+  border-radius: 15px;
+  box-shadow: 2px 2px 5px 0;
+  display: grid;
+  place-items: center;
+  padding: auto;
+  
+}
+
+.caja4 {
+  border-radius: 15px;
+  box-shadow: 2px 2px 5px 0;
+  padding: auto;
+}
+
+.caja4 p {
+  text-align: left;
+}
+.caja5 {
+  border-radius: 15px;
+  box-shadow: 2px 2px 5px 0;
+  padding: auto;
+}
+
+.caja5 p {
+  text-align: left;
+}
+
+.caja6 {
+  border-radius: 15px;
+  box-shadow: 2px 2px 5px 0;
+  width: 700px;
+  padding: auto;
+}
+
+
+
+.caja8 {
+  margin-left: 30px;
+  padding: auto;
+}
+
+h2 {
+  margin-top: 15px;
+  color: gray;
+  font-family: "Quicksand", sans-serif;
+  text-align: center;
+}
+
+h3 {
+  color: gray;
+  font-family: "Quicksand", sans-serif;
+  font-size: 20px;
+  text-align: center;
+}
+img {
+  height: 20px;
+  width: auto;
+  margin-left: 12px;
+  margin-top: 6px;
+}
+
+.vue {
+  height: 80px;
+  width: auto;
+  margin-left: 6px;
+  margin-top: 6px;
+}
+
+li {
+  color: gray;
+  font-size: 14px;
+  font-family: "Quicksand", sans-serif;
+}
+
+p {
+  font-size: 14px;
+  color: gray;
+  font-family: "Quicksand", sans-serif;
+}
+
+div.modal-content {
+  border-radius: 15px;
+  box-shadow: 2px 2px 5px 0;
+  padding: auto;
+  background-color: #eefce2;
+  font-size: 14px;
+  color: gray;
+  font-family: "Quicksand", sans-serif;
+}
+
+.form-label {
+  font-size: 14px;
+  margin-top: 0px;
+}
+
+.form-label-2 {
+  font-size: 14px;
+  margin-top: 10px;
+}
+
+.form-text {
+  font-size: 11px;
+} 
+
+button {
+  background-color: #ddf7f2;
+  font-size: 10px;
+  border-radius: 2px;
+  box-shadow: 2px 2px 5px 0;
+  height: 25px;
+  margin-bottom: 15px;
+  color: gray;
+  font-family: "Quicksand", sans-serif;
+  
+}
+
+button.btn.btn-light {
+  background-color: #eed5ee;
+  font-size: 10px;
+  border-radius: 2px;
+  box-shadow: 2px 2px 5px 0;
+  height: 25px;
+  margin-bottom: 15px;
+  margin-top: 6px;
+  margin-left: 220px;
+  color: gray;
+  font-family: "Quicksand", sans-serif;
+}
+
+footer {
+  /* flex-shrink: 0; */
+  padding: 20px 124px 5px 124px;
+  background-color: #fcfce2;
+  /* margin-top: 20px; */
+  box-shadow: 2px 2px 5px 0;
+  /* border-radius: 10px; */
+  /* width: 1420px; */
+  /* margin: 0;
+  border: 0; */
+  width: 100vw;
+  border: 1px red solid;
+}
+
+form {
+  display: flex;
+  align-content: space-between;
+  justify-content: center;
+  width: 100vw;
+}
+
+input {
+  height: 25px;
+}
+.inputlargo {
+  height: 105px;
+}
+.form-label {
+  font-size: 14px;
+  margin-top: 0px;
+  font-family: "Quicksand", sans-serif;
+  color: gray;
+}
+.form-label-2 {
+  font-size: 14px;
+  margin-top: 10px;
+  font-family: "Quicksand", sans-serif;
+  color: gray;
+}
+.form-text {
+  font-size: 11px;
+  font-family: "Quicksand", sans-serif;
+}
+
+.contacto {
+  font-size: 14px;
+  margin-top: 50px;
+  color: gray;
+}
+
+p {
+  margin-top: 20px;
+  font-size: 14px;
+  color: gray;
+}
+</style>
